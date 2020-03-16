d5cf5a3f499f8afb9d544cfb533c3775879423a2
Matthias Haegele <matthias.haegele@mhp.com>
2020-03-16 14:51:27 +0100

    Test PDS Components Mock | mh

--- src/App.js
+++ src/App.js
@@ -1,32 +1,195 @@
 import React from 'react';
-import { PHeadline, PIcon, PButton } from '@porsche-design-system/components-react';
+import {
+    PButton,
+    PCheckboxWrapper,
+    PGrid,
+    PGridItem,
+    PHeadline,
+    PSelectWrapper,
+    PTextareaWrapper,
+    PFlex,
+    PFlexItem,
+    PTextFieldWrapper,
+    PRadioButtonWrapper,
+    PButtonPure,
+    PLink,
+    PLinkPure,
+    PSpinner,
+    PIcon,
+    PPagination
+} from '@porsche-design-system/components-react';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      submitted: false
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            submitted: false,
+            selectValue: 'Change this Headline by selecting',
+            textFieldValue: 'Change this Headline by typing',
+            checked: false,
+            radioChecked: false
+        };
+        this.handleChangeSelect = this.handleChangeSelect.bind(this);
+        this.handleChangeTextField = this.handleChangeTextField.bind(this);
+    }
 
-  submit(event) {
-    event.nativeEvent.preventDefault();
-    this.setState({
-      submitted: true
-    });
-  }
+    submit(event) {
+        event.nativeEvent.preventDefault();
+        this.setState({
+            submitted: true
+        });
+    }
 
-  render() {
-    return (
-      <div className="App">
-        { this.state.submitted ? <PHeadline>Hello</PHeadline> : '' }
-        <PIcon name="delete"></PIcon>
-        <form onSubmit={e => this.submit(e)}>
-          <PButton type={"submit"}>Submit</PButton>
-        </form>
-      </div>
-    );
-  }
+    check(event) {
+        event.nativeEvent.preventDefault();
+        this.setState({
+            checked: true
+        });
+    }
+
+    radioCheck(event) {
+        event.nativeEvent.preventDefault();
+        this.setState({
+            radioChecked: true
+        });
+    }
+
+    dismiss(event) {
+        event.nativeEvent.preventDefault();
+        this.setState({submitted: false});
+    }
+
+    handleChangeSelect(event) {
+        this.setState({selectValue: event.target.value})
+    }
+
+    handleChangeTextField(event) {
+        this.setState({textFieldValue: event.target.value})
+    }
+
+    getHeadline(stateValue) {
+        const headline = stateValue;
+
+        return headline;
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <PGrid>
+                    <PGridItem size={12}>
+                        <PHeadline variant={"headline-2"}>Example Projekt to display useage of Porsche Design
+                            System</PHeadline>
+                        <br/>
+                        <hr/>
+                        <br/>
+                    </PGridItem>
+                    {/*To illustrate the mock procedure during the tests the buttons insert/dismiss a headline*/}
+                    {this.state.submitted ? <PHeadline variant={"headline-4"}>Hello</PHeadline> : ''}
+
+                    <PGridItem>
+                        <form onSubmit={e => this.submit(e)}>
+                            <PButton type={"submit"}>Submit</PButton>
+                        </form>
+                        <br/>
+                        <form onSubmit={e => this.dismiss(e)}>
+                            <PButtonPure type={"submit"}>Dismiss</PButtonPure>
+                        </form>
+                    </PGridItem>
+                </PGrid>
+                <PFlex direction={"column"}>
+                    <PFlexItem>
+                        <br/>
+                        <hr/>
+                        <br/>
+                        <PHeadline variant={"headline-3"}>Form examples</PHeadline>
+                        <br/>
+                        <hr/>
+                        <br/>
+                        <PHeadline variant={"headline-4"}>{this.getHeadline(this.state.selectValue)}</PHeadline>
+                        <form>
+                            <PSelectWrapper>
+                                <select data-testid="select" value={this.state.selectValue}
+                                        onChange={this.handleChangeSelect}>
+                                    <option value={"Headline A"}>Headline A</option>
+                                    <option value={"Headline B"}>Headline B</option>
+                                    <option value={"Headline C"}>Headline C</option>
+                                </select>
+                            </PSelectWrapper>
+                        </form>
+                    </PFlexItem>
+                    <PFlexItem>
+                        <br/>
+                        <hr/>
+                        <br/>
+                        {this.state.checked ? <PHeadline variant={"headline-4"}>Checkbox Works</PHeadline> : ''}
+                        <form onInput={e => this.check(e)}>
+                            <PCheckboxWrapper label={"Some label"} hideLabel={false}>
+                                <input data-testid="checkbox" type={"checkbox"} name={"TestBox"}></input>
+                            </PCheckboxWrapper>
+                        </form>
+                        <br/>
+                        {this.state.radioChecked ? <PHeadline variant={"headline-4"}>Radio Works</PHeadline> : ''}
+                        <form onInput={e => this.radioCheck(e)}>
+                            <PRadioButtonWrapper label={"Some label"} hideLabel={false}>
+                                <input data-testid="radiobutton" type={"radio"} name={"RadioButton"}></input>
+                            </PRadioButtonWrapper>
+                        </form>
+                    </PFlexItem>
+                    <PFlexItem>
+                        <br/>
+                        <hr/>
+                        <br/>
+                        <form>
+                            <PTextareaWrapper label={"Test TextArea"} hideLabel={false}>
+                                <textarea name={"Testarea"}></textarea>
+                            </PTextareaWrapper>
+                        </form>
+                        <br/>
+                    </PFlexItem>
+                    <PFlexItem>
+                        <PHeadline variant={"headline-4"}>{this.getHeadline(this.state.textFieldValue)}</PHeadline>
+                        <form>
+                            <PTextFieldWrapper label={"Test TextField"} hideLabel={false}>
+                                <input data-testid="input" type={"text"} name={"Textfield"}
+                                       onChange={this.handleChangeTextField}></input>
+                            </PTextFieldWrapper>
+                        </form>
+                    </PFlexItem>
+                    <PFlexItem>
+                        <br/>
+                        <hr/>
+                        <br/>
+                        <PHeadline variant={"headline-4"}>Links</PHeadline>
+                        <br/>
+                    </PFlexItem>
+                    <PFlexItem>
+                        <a href="https://www.porsche.com" target="_blank" rel="noopener noreferrer">
+                            <PLink variant={"primary"}>porsche.com</PLink>
+                        </a>
+                    </PFlexItem>
+                    <br/>
+                    <PFlexItem>
+                        <a href="https://www.porsche.com" target="_blank" rel="noopener noreferrer">
+                            <PLinkPure>porsche.com</PLinkPure>
+                        </a>
+                        <PLinkPure href="https://www.porsche.com">Porsche.Com</PLinkPure>
+                    </PFlexItem>
+                    <PFlexItem>
+                        <br/>
+                        <hr/>
+                        <br/>
+                        <PSpinner size={"small"} ariaLabel={"Loading"}></PSpinner>
+                    </PFlexItem>
+                    <PFlexItem>
+                        <PIcon size={"medium"} name={"highway"} ariaLabel={"Highway icon"} role={"img"}></PIcon>
+                    </PFlexItem>
+                    <PPagination totalItemsCount={9} itemsPerPage={3} activePage={1}
+                                 maxNumberOfPageLinks={4}></PPagination>
+                </PFlex>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,17 +1,74 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render, fireEvent} from '@testing-library/react';
 import App from './App';
 
 jest.mock('@porsche-design-system/components-react', () => ({
-  PHeadline: props => <mock-PHeadline>{ props.children }</mock-PHeadline>,
-  PIcon: () => <mock-PIcon></mock-PIcon>,
-  PButton: props => <button>{ props.children }</button>
+    PHeadline: props => <mock-PHeadline>{props.children}</mock-PHeadline>,
+    PIcon: () => <mock-PIcon></mock-PIcon>,
+    PButton: props => <button>{props.children}</button>,
+    PButtonPure: props => <button>{props.children}</button>,
+    PGrid: props => <div>{props.children}</div>,
+    PGridItem: props => <div>{props.children}</div>,
+    PFlex: props => <div>{props.children}</div>,
+    PFlexItem: props => <div>{props.children}</div>,
+    PSelectWrapper: props => <mock-PSelectWrapper>{props.children}</mock-PSelectWrapper>,
+    PCheckboxWrapper: props => <mock-PCheckboxWrapper>{props.children}</mock-PCheckboxWrapper>,
+    PRadioButtonWrapper: props => <mock-PRadioButtonWraper>{props.children}</mock-PRadioButtonWraper>,
+    PTextareaWrapper: () => <mock-PTextareaWrapper></mock-PTextareaWrapper>,
+    PTextFieldWrapper: props => <mock-PTextFieldWrapper>{props.children}</mock-PTextFieldWrapper>,
+    PLink: () => <mock-PLink></mock-PLink>,
+    PLinkPure: () => <mock-PLinkPure></mock-PLinkPure>,
+    PSpinner: () => <mock-PSpinner></mock-PSpinner>,
+    PPagination: props => <mock-Pagination>{props.children}</mock-Pagination>
 }));
 
 test('renders a headline from Porsche Design System', async () => {
-  const { getByText } = render(<App />);
-  const submitButtonElement = getByText(/Submit/i);
-  submitButtonElement.click();
-  const headLineElement = getByText(/Hello/i);
-  expect(headLineElement).toBeInTheDocument();
+    const {getByText} = render(<App/>);
+    const submitButtonElement = getByText(/Submit/i);
+    submitButtonElement.click();
+    const headLineElement = getByText(/Hello/i);
+    expect(headLineElement).toBeInTheDocument();
+});
+
+test('dissmisses the headline from Porsche Design System', async () => {
+    const {getByText} = render(<App/>);
+    const submitButtonElement = getByText(/Submit/i);
+    submitButtonElement.click();
+    const headLineElement = getByText(/Hello/i);
+
+    const dismissButtonElement = getByText(/Dismiss/i);
+    dismissButtonElement.click();
+    expect(headLineElement).not.toBeInTheDocument();
+});
+
+test('headline should be changed according the selected value', async () => {
+    const {getByText, getByTestId} = render(<App/>);
+    expect(getByText("Change this Headline by selecting")).toBeInTheDocument();
+    fireEvent.change(getByTestId("select", { target: { value: 'Headline B' } }));
+    expect(getByText("Headline B")).toBeInTheDocument();
+    fireEvent.change(getByTestId("select", { target: { value: 'Headline C' } }));
+    expect(getByText("Headline C")).toBeInTheDocument();
+});
+
+test('headline should be displayed after click on Checkbox', async () => {
+    const {getByText, getByTestId} = render(<App/>);
+    const input = getByTestId('checkbox');
+    input.click();
+    expect(getByText("Checkbox Works")).toBeInTheDocument();
+});
+
+test('headline should be displayed after click on RadioButton', async () => {
+    const {getByText, getByTestId} = render(<App/>);
+    const input = getByTestId('radiobutton');
+    input.click();
+    expect(getByText("Radio Works")).toBeInTheDocument();
+});
+
+test('headline should be changed according the typed value', async () => {
+    const {getByText, getByTestId} = render(<App/>);
+    expect(getByText("Change this Headline by typing")).toBeInTheDocument();
+    fireEvent.change(getByTestId("input", { target: { value: 'Headline A' } }));
+    expect(getByText("Headline A")).toBeInTheDocument();
+    fireEvent.change(getByTestId("select", { target: { value: 'Headline B' } }));
+    expect(getByText("Headline B")).toBeInTheDocument();
 });
