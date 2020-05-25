51e70ed6a01f1c3753f4fc79f98f9898b162cfcf
Bernd Hacker <hacker.bernd@gmail.com>
2020-05-25 12:15:50 +0200

    feat: add some other components on same page as phn-header | bh

--- src/pages/PageWithPhnHeader.tsx
+++ src/pages/PageWithPhnHeader.tsx
@@ -1,5 +1,26 @@
 import React from "react";
+import {
+  PButton,
+  PIcon,
+  PLinkPure,
+} from "@porsche-design-system/components-react";
 
-export const PageWithPhnHeader = () => {
-  return <phn-header />;
-};
+export const PageWithPhnHeader = () => (
+  <>
+    <phn-header />
+
+    <div style={{ paddingTop: 20 }}>
+      <div>
+        <PButton>Some PButton</PButton>
+      </div>
+      <div>
+        <PLinkPure>Some PLinkPure</PLinkPure>
+      </div>
+      <div>
+        <PIcon name="configurate" />
+        <PIcon name="compare" />
+        <PIcon name="search" />
+      </div>
+    </div>
+  </>
+);
