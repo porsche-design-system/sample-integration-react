d8d9b69af37830c423c4c9f679bcc2442976eb24
Bernd Hacker <hacker.bernd@gmail.com>
2020-05-25 12:42:38 +0200

    feat: align layout and classnames with angular example integration | bh

--- src/App.tsx
+++ src/App.tsx
@@ -21,46 +21,43 @@ export function App() {
 
     return (
         <Router>
-            <div className="App">
-                <div className={'pageLayout'}>
-                    <PGrid>
-                        <PGridItem size={12}>
-                            <PHeadline variant={"headline-2"}>Example Project to display usage of Porsche Design
-                                System</PHeadline>
-                        </PGridItem>
-                        <PGridItem size={12}>
-                            <PDivider className={'divider'}/>
-                        </PGridItem>
-                        <PGridItem size={12}>
-                            <Link to="/componentsCollection" className={'removeLinkStyle'}>
-                                <PLinkPure>Components Collection</PLinkPure>
-                            </Link>
-                            <Link to="/forms" className={'removeLinkStyle'}>
-                                <PLinkPure>Forms</PLinkPure>
-                            </Link>
-                            <Link to="/phn-header" className={'removeLinkStyle'}>
-                                <PLinkPure>Phn Header</PLinkPure>
-                            </Link>
-                        </PGridItem>
-                        <PGridItem size={12}>
-                            <PDivider className={'divider'}/>
-                        </PGridItem>
-                    </PGrid>
-                    <Switch>
-                        <Route path="/" exact>
-                            <ComponentsCollection/>
-                        </Route>
-                        <Route path="/componentsCollection">
-                            <ComponentsCollection/>
-                        </Route>
-                        <Route path="/forms">
-                            <Forms/>
-                        </Route>
-                        <Route path="/phn-header">
-                            <PageWithPhnHeader/>
-                        </Route>
-                    </Switch>
-                </div>
+            <div className={'pageLayout'}>
+                <PGrid>
+                    <PGridItem size={12}>
+                        <PHeadline variant="headline-2" align="center">Example usage of the Porsche-Design-System React</PHeadline>
+                    </PGridItem>
+                    <PGridItem size={12}>
+                        <PDivider className={'divider'}/>
+                    </PGridItem>
+                    <PGridItem size={12}>
+                        <Link to="/componentsCollection" className={'removeLinkStyle'}>
+                            <PLinkPure>Components Collection</PLinkPure>
+                        </Link>
+                        <Link to="/forms" className={'removeLinkStyle'}>
+                            <PLinkPure>Forms</PLinkPure>
+                        </Link>
+                        <Link to="/phn-header" className={'removeLinkStyle'}>
+                            <PLinkPure>Phn Header</PLinkPure>
+                        </Link>
+                    </PGridItem>
+                    <PGridItem size={12}>
+                        <PDivider className={'divider'}/>
+                    </PGridItem>
+                </PGrid>
+                <Switch>
+                    <Route path="/" exact>
+                        <ComponentsCollection/>
+                    </Route>
+                    <Route path="/componentsCollection">
+                        <ComponentsCollection/>
+                    </Route>
+                    <Route path="/forms">
+                        <Forms/>
+                    </Route>
+                    <Route path="/phn-header">
+                        <PageWithPhnHeader/>
+                    </Route>
+                </Switch>
             </div>
         </Router>
     );
