80f7fdf7f44a08387a07414d34034c95d57512c2
Bernd Hacker <hacker.bernd@gmail.com>
2020-05-29 11:55:48 +0200

    refactor: apply polyfills for @porschehn/navigation in PhnHeaderPage | bh

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 import * as serviceWorker from './serviceWorker';
-import { applyPolyfills, defineCustomElements as definePorscheNavigation } from '@porschehn/navigation/loader';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
@@ -12,7 +11,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
-applyPolyfills().then(() => {
-  definePorscheNavigation(window);
-});
