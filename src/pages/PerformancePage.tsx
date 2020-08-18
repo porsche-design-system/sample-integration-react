b9d7ab08f26602aa928c11180e65d2366d67ab69
Matthias Haegele <matthias.haegele@mhp.com>
2020-08-18 09:06:05 +0200

    feat: change styling | mh | 874

--- src/pages/PerformancePage.tsx
+++ src/pages/PerformancePage.tsx
@@ -29,37 +29,45 @@ export const PerformancePage = () => {
         </PCheckboxWrapper>
       );
     } else if (element === 'button') {
-      return <PButton variant="primary" key={'P' + count}>Lorem Ipsum {count}</PButton>;
+      return (
+        <PButton variant="primary" key={'P' + count}>
+          Lorem Ipsum {count}
+        </PButton>
+      );
     } else return <PText key={'P' + count} children={'Lorem Ipsum ' + count} />;
   };
 
   return (
-    <PGrid>
-      <PGridItem size="12">
-        <PHeadline variant="headline-4">Choose Element and Count</PHeadline>
-      </PGridItem>
-      <PGridItem size="2">
-        <PSelectWrapper>
-          <select value={element} onChange={(e) => changeElement(e)}>
-            <option value="text">text</option>
-            <option value="checkbox">checkbox</option>
-            <option value="button">button</option>
-          </select>
-        </PSelectWrapper>
-      </PGridItem>
-      <PGridItem size="2">
-        <PSelectWrapper>
-          <select value={count} onChange={(e) => changeCount(e)}>
-            <option value="0">0</option>
-            <option value="25">25</option>
-            <option value="50">50</option>
-            <option value="100">100</option>
-          </select>
-        </PSelectWrapper>
-      </PGridItem>
-      <PGridItem size="12" style={{ marginTop: '2rem' }}>
-        {Array.from(Array(count)).map((value, i) => chooseElement(i))}
-      </PGridItem>
-    </PGrid>
+    <>
+      <PGrid>
+        <PGridItem size="12">
+          <PHeadline variant="headline-4">Choose Element and Count</PHeadline>
+        </PGridItem>
+        <PGridItem size="2">
+          <PSelectWrapper>
+            <select value={element} onChange={(e) => changeElement(e)}>
+              <option value="text">text</option>
+              <option value="checkbox">checkbox</option>
+              <option value="button">button</option>
+            </select>
+          </PSelectWrapper>
+        </PGridItem>
+        <PGridItem size="2">
+          <PSelectWrapper>
+            <select value={count} onChange={(e) => changeCount(e)}>
+              <option value="0">0</option>
+              <option value="25">25</option>
+              <option value="50">50</option>
+              <option value="100">100</option>
+            </select>
+          </PSelectWrapper>
+        </PGridItem>
+      </PGrid>
+      <PGrid style={{ marginTop: '2rem' }}>
+        <PGridItem size="4">
+          {Array.from(Array(count)).map((value, i) => chooseElement(i))}
+        </PGridItem>
+      </PGrid>
+    </>
   );
 };
