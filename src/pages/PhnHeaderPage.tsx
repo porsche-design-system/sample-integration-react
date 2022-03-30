2a34e10176c462637a8871ca389eacbbd4e7f12f
Matthias Haegele <matthias.haegele@mhp.com>
2022-03-30 08:25:45 +0200

    fix: add href onto link | mh

--- src/pages/PhnHeaderPage.tsx
+++ src/pages/PhnHeaderPage.tsx
@@ -9,7 +9,7 @@ export const PhnHeaderPage = (): JSX.Element => (
         <PButton>Some PButton</PButton>
       </div>
       <div>
-        <PLinkPure>Some PLinkPure</PLinkPure>
+        <PLinkPure href="#">Some PLinkPure</PLinkPure>
       </div>
       <div>
         <PIcon name="configurate" />
