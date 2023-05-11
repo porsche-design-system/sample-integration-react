aedde5aef3e8e9525cfe77969beb34bb0cc0ad05
TatjanaGartner <tatjana.gartner@mhp.com>
2023-05-11 11:49:29 +0200

    test: update unit tests | tg

--- src/tests/FormsPage.test.tsx
+++ src/tests/FormsPage.test.tsx
@@ -4,19 +4,19 @@ import { FormsPage } from '../pages';
 import { renderWithProvider } from './helper';
 
 describe('FormsPage', () => {
-  it('headline should be changed according the selected value', async () => {
+  it('heading should be changed according the selected value', async () => {
     const { getByText, getByTestId } = renderWithProvider(<FormsPage />);
     await componentsReady();
-    expect(getByText('Change this Headline by selecting')).toBeInTheDocument();
+    expect(getByText('Change this Heading by selecting')).toBeInTheDocument();
 
-    fireEvent.change(getByTestId('select'), { target: { value: 'Headline B' } });
-    expect(getByText('Headline B')).toBeInTheDocument();
+    fireEvent.change(getByTestId('select'), { target: { value: 'Heading B' } });
+    expect(getByText('Heading B')).toBeInTheDocument();
 
-    fireEvent.change(getByTestId('select'), { target: { value: 'Headline C' } });
-    expect(getByText('Headline C')).toBeInTheDocument();
+    fireEvent.change(getByTestId('select'), { target: { value: 'Heading C' } });
+    expect(getByText('Heading C')).toBeInTheDocument();
   });
 
-  it('headline should be displayed after click on Checkbox', async () => {
+  it('heading should be displayed after click on Checkbox', async () => {
     const { getByText, getByTestId } = renderWithProvider(<FormsPage />);
     await componentsReady();
     const input = getByTestId('checkbox');
@@ -25,7 +25,7 @@ describe('FormsPage', () => {
     expect(getByText('Checkbox Works')).toBeInTheDocument();
   });
 
-  it('headline should be displayed after click on RadioButton', async () => {
+  it('heading should be displayed after click on RadioButton', async () => {
     const { getByText, getByTestId } = renderWithProvider(<FormsPage />);
     await componentsReady();
     const input = getByTestId('radiobutton');
@@ -34,16 +34,16 @@ describe('FormsPage', () => {
     expect(getByText('Radio Works')).toBeInTheDocument();
   });
 
-  it('headline should be changed according the typed value', async () => {
+  it('heading should be changed according the typed value', async () => {
     const { getByText, getByTestId } = renderWithProvider(<FormsPage />);
     await componentsReady();
-    getByText('Change this Headline by typing');
+    getByText('Change this Heading by typing');
 
-    fireEvent.change(getByTestId('input'), { target: { value: 'Headline C' } });
-    getByText('Headline C');
+    fireEvent.change(getByTestId('input'), { target: { value: 'Heading C' } });
+    getByText('Heading C');
 
-    fireEvent.change(getByTestId('input'), { target: { value: 'Headline B' } });
-    getByText('Headline B');
+    fireEvent.change(getByTestId('input'), { target: { value: 'Heading B' } });
+    getByText('Heading B');
   });
 
   /* jsdom has some limitations. One of them is the fact that we cant change location. But we are able to test the closest href */
