6647a788042c44567afed84bce2e38e48c70d9cc
Michael Worm <michael.worm@mhp.com>
2024-06-11 10:11:46 +0200

    chore: Test React 19 RC update

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,20 @@
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
 import { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import { App } from './App';
 import { applyPolyfills, defineCustomElements as definePorscheNavigation } from '@porschehn/navigation/loader';
 import { PorscheDesignSystemProvider } from '@porsche-design-system/components-react'
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement!);
+
+root.render(
   <StrictMode>
   <PorscheDesignSystemProvider>
     <App />
   </PorscheDesignSystemProvider>
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
 
 applyPolyfills().then(() => {
